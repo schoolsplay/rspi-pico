d0e9faa0445d332f31fc957ade519189816978bd
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2021-02-12 21:44:39 +0100

    Added latest micropython firmware. Added pip requirements Updated various test scripts

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ led_onboard = machine.Pin(25, machine.Pin.OUT)
 
 def main():
     while True:
-        print("blink")
+        print("blink, print")
         led_onboard.value(1)
         utime.sleep(1)
         led_onboard.value(0)
