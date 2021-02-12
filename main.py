74525d6b64b8aa2293089b1b2ea50b6c5c8561a8
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2021-02-12 22:15:26 +0100

    Added docstring to the main starter

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+"""
+Starter for your script.
+Be aware that this uses the buildin LED at pin 25:
+    constant green -> module is imported correctly
+    constant red at pin 2 -> import or calling module.main failed
+
+It will
+"""
+
 import os
 import machine
+import utime
 
 logfile = "main.log"
 try:
     os.remove(logfile)
 except:
     pass
-
-led_green = machine.Pin(2, machine.Pin.OUT)
-led_red  = machine.Pin(2, machine.Pin.OUT)
 
 def write_log(txt):
     """Use only in development!
@@ -17,31 +24,37 @@ def write_log(txt):
         f.write(txt + '\n')
 
 def reset_led():
+    machine.Pin(25, machine.Pin.OUT).value(0)
     machine.Pin(2, machine.Pin.OUT).value(0)
-    machine.Pin(3, machine.Pin.OUT).value(0)
 
-def set_led(color="green"):
-    write_log("set_led called with: {}".format(color))
+def set_led(ok):
+    #write_log("set_led called with: {}".format(state))
     reset_led()
-    if color == "green":
-        led = machine.Pin(2, machine.Pin.OUT)
+
+    if ok:
+        machine.Pin(25, machine.Pin.OUT).value(1)
     else:
-        led = machine.Pin(3, machine.Pin.OUT)
-    led.value(1)
+        machine.Pin(2, machine.Pin.OUT).value(1)
 
 reset_led()
 
 try:
     import test as module
     #import test_uart as module
-except (Exception, KeyboardInterrupt) as e:
+except Exception as e:
     print("Exception in main importing script")
     print(e)
-    set_led('red')
+    set_led(False)
 else:
     print("Import script successful")
-    set_led()
-    module.main()
+    set_led(True)
+    try:
+        module.main()
+    except Exception as e:
+        print("Exception in module.main")
+        print(e)
+        set_led(False)
+
 
 print("End of main")
 
--- test.py
+++ test.py
@@ -2,12 +2,12 @@
 import machine
 import utime
 
-led_onboard = machine.Pin(25, machine.Pin.OUT)
+led = machine.Pin(3, machine.Pin.OUT)
 
 def main():
     while True:
         print("blink, print")
-        led_onboard.value(1)
+        led.value(1)
         utime.sleep(1)
-        led_onboard.value(0)
+        led.value(0)
         utime.sleep(1)
